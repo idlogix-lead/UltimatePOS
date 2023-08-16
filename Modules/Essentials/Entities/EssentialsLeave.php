d988314f47e7ec89d1cd9537c590f5e852a99502
M Haris Maqsood <pk.harissultani@gmail.com>
2023-08-16 10:32:09 +0500

    Essentials and Hrm Module added

---
+++ Modules/Essentials/Entities/EssentialsLeave.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\Essentials\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class EssentialsLeave extends Model
+{
+    // haris commented this
+    // use LogsActivity;
+
+    protected static $logAttributes = ['*'];
+
+    protected static $logAttributesToIgnore = [ 'created_at', 'updated_at'];
+
+    protected static $logOnlyDirty = true;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\User::class);
+    }
+
+    public function leave_type()
+    {
+        return $this->belongsTo(\Modules\Essentials\Entities\EssentialsLeaveType::class, 'essentials_leave_type_id');
+    }
+
+    public function changed_by_user()
+    {
+        return $this->belongsTo(\App\User::class, 'changed_by');
+    }
+}
