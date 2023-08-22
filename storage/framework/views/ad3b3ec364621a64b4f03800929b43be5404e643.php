7a4042465802cc626979aed09b5d28cf1a8bf4c8
M Haris Maqsood <pk.harissultani@gmail.com>
2023-08-22 13:47:42 +0500

    ff

---
+++ storage/framework/views/ad3b3ec364621a64b4f03800929b43be5404e643.php
@@ -0,0 +1,2 @@
+    </body>
+</html><?php /**PATH E:\xampp8.2\htdocs\UltimatePOS\resources\views/report/partials/api/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d91f6f85e11d88cf769e285934cd93e928459a2e.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Report</title>
+</head>
+<body>
+    <div style="width:100%;line-height: 1;">
+        <!-- <div class="" style="text-align:right;">
+            <span style="text-align:left;"></span>
+            <span ></span>
+        </div> -->
+        <table style="width:100%;border:0px solid #ddd;">
+            <tr>
+                <td style="text-align:left;border:0px solid #ddd;"><label for="">Printed By: </label> <?php echo e($data["user"], false); ?></td>
+                <td style="text-align:right;border:0px solid #ddd;"><label for="">Date Range: </label> <?php echo e(date($data["date_format"],strtotime($data["start_date"])), false); ?> - <?php echo e(date($data["date_format"],strtotime($data["end_date"])), false); ?></td>
+            </tr>
+        </table>
+        
+        <div style="text-align:center;">
+            <h2><label for="">Business: </label><?php echo e($data["name"], false); ?></h2>
+            <h4><label for="">Location: </label><?php echo e($data["location"]["name"], false); ?>(<?php echo e($data["location"]["location_id"], false); ?>)</h4>
+        </div>
+    </div>
+<?php /**PATH E:\xampp8.2\htdocs\UltimatePOS\resources\views/report/partials/api/header.blade.php ENDPATH**/ ?>
