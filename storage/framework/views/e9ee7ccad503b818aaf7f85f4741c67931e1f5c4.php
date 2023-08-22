20d90c494967843cf39a41b7515e0a48495b92bf
M Haris Maqsood <pk.harissultani@gmail.com>
2023-08-22 13:47:20 +0500

    fixes

---
+++ storage/framework/views/e9ee7ccad503b818aaf7f85f4741c67931e1f5c4.php
@@ -0,0 +1,4 @@
+<div id="<?php echo e($chart->id, false); ?>" <?php echo $chart->formatContainerOptions('css'); ?>>
+</div>
+<?php echo $__env->make('charts::loader', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php /**PATH E:\xampp8.2\htdocs\UltimatePOS\vendor\consoletvs\charts\src/Views/highcharts/container.blade.php ENDPATH**/ ?>
