7e053127663c8e4e7cce699b398a4cbabb55a446
M Haris Maqsood <pk.harissultani@gmail.com>
2023-08-31 11:07:36 +0500

    kj

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -97,6 +97,7 @@ class ApiController extends Controller
         $file_path = 'app\api_report'.$path."\\".$file;
         return Response::make(file_get_contents(storage_path($file_path)), 200, [
             'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="'.$file.'"'
         ]);
     }
 
